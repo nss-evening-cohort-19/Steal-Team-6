689e840eadc0299f419a2b9e5fe70b9e33785133
Emily Stroud <emilystroud93@gmail.com>
2022-08-03 19:27:43 -0500

    created list API calls

--- api/listData.js
+++ api/listData.js
@@ -1,1 +1,51 @@
-// test //
+import axios from 'axios';
+import { clientCredentials } from '../utils/client';
+
+const dbUrl = clientCredentials.databaseURL;
+
+const getList = (uid) => new Promise((resolve, reject) => {
+  axios.get(`${dbUrl}/list.json?orderBy="uid"&equalTo="${uid}"`)
+    .then((response) => {
+      if (response.data) {
+        resolve(Object.values(response.data));
+      } else {
+        resolve([]);
+      }
+    })
+    .catch((error) => reject(error));
+});
+
+const deleteList = (firebaseKey) => new Promise((resolve, reject) => {
+  axios.delete(`${dbUrl}/list/${firebaseKey}.json`)
+    .then(() => resolve('deleted'))
+    .catch((error) => reject(error));
+});
+
+const getSingleList = (firebaseKey) => new Promise((resolve, reject) => {
+  axios.get(`${dbUrl}/list/${firebaseKey}.json`)
+    .then((response) => resolve(response.data))
+    .catch((error) => reject(error));
+});
+
+const createList = (listObj) => new Promise((resolve, reject) => {
+  axios.post(`${dbUrl}/list.json`, listObj)
+    .then((response) => {
+      const payload = { firebaseKey: response.data.name };
+      axios.patch(`${dbUrl}/list/${response.data.name}.json`, payload)
+        .then(resolve);
+    }).catch(reject);
+});
+
+const updateList = (listObj) => new Promise((resolve, reject) => {
+  axios.patch(`${dbUrl}/list/${listObj.firebaseKey}.json`, listObj)
+    .then(resolve)
+    .catch(reject);
+});
+
+export {
+  getList,
+  createList,
+  deleteList,
+  getSingleList,
+  updateList,
+};
--- components/ListCard.js
+++ components/ListCard.js
@@ -1,1 +1,13 @@
-// test //
+// import { deleteList } from '../api/listData';
+
+// function ListCard({ listObj, onUpdate }) {
+//   const deleteThisList = () => {
+//     if (window.confirm(`Delete ${listObj.name}?`)) {
+//       deleteList(listObj.firebaseKey).then(() => onUpdate());
+//     }
+//   };
+
+//   return (
+
+//   )
+// }
