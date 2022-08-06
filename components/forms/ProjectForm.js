4bf6f202fb501b758f04d76b9c93525ace693e18
Adam Steel <asteel88@gmail.com>
2022-08-06 09:06:11 -0500

    Project Form

--- components/forms/ProjectForm.js
+++ components/forms/ProjectForm.js
@@ -1,1 +1,86 @@
-// test //
+import React, { useEffect, useState } from 'react';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import PropTypes from 'prop-types';
+import { useAuth } from '../../utils/context/authContext';
+import { createProject, updateProject } from '../../api/projectData';
+
+const initialState = {
+  title: '',
+  public: false,
+};
+
+function ProjectForm({ obj }) {
+  const [formInput, setFormInput] = useState(initialState);
+  const router = useRouter();
+  const { user } = useAuth();
+
+  useEffect(() => {
+    if (obj.firebaseKey) setFormInput(obj);
+  }, [obj, user]);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormInput((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (obj.firebaseKey) {
+      updateProject(formInput)
+        .then(() => router.push(`/project/${obj.firebaseKey}`));
+    } else {
+      const payload = { ...formInput, uid: user.uid };
+      createProject(payload).then(() => {
+        router.push('/');
+      });
+    }
+  };
+
+  return (
+    <>
+      <Head>
+        <title>Creat a new Project!</title>
+        <meta name="description" content="Meta description for the team page" />
+      </Head>
+      <form onSubmit={handleSubmit}>
+        <h2 className="text-white mt-5">{obj.firebaseKey ? 'Update' : 'Create'} New Project</h2>
+        <label htmlFor="floatingInput1" label="Team Name" className="mb-3">
+          <form type="text" placeholder="Enter your project name" name="name" value={formInput.title} onChange={handleChange} required />
+        </label>
+
+        {/* A WAY TO HANDLE UPDATES FOR TOGGLES, RADIOS, ETC  */}
+        <form
+          className="text-white mb-3"
+          type="switch"
+          id="public"
+          name="public"
+          label="Public Project?"
+          checked={formInput.public}
+          onChange={(e) => setFormInput((prevState) => ({
+            ...prevState,
+            public: e.target.checked,
+          }))}
+        />
+        <button className="btn btn-primary btn-lg copy-btn" type="submit">{obj.firebaseKey ? 'Update' : 'Create'} Project</button>
+      </form>
+    </>
+  );
+}
+
+ProjectForm.propTypes = {
+  obj: PropTypes.shape({
+    title: PropTypes.string,
+    public: PropTypes.bool,
+    firebaseKey: PropTypes.string,
+  }),
+};
+
+ProjectForm.defaultProps = {
+  obj: initialState,
+};
+
+export default ProjectForm;
