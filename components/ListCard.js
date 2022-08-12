bbcb1cc8e29374c9023dffc630f2b2c3cb656e54
Keaton Law <keaton.c.law@gmail.com>
2022-08-12 17:59:33 -0500

    added map to lists

--- components/ListCard.js
+++ components/ListCard.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
 import { Card, Button } from 'react-bootstrap';
 
 import { deleteListCards } from '../api/mergedData';
+import { getListCards } from '../api/listData';
 
 function ListCard({ listObj, onUpdate }) {
+  const [card, setCard] = useState([]);
+
+  useEffect(() => {
+    getListCards(listObj.firebaseKey).then(setCard);
+  }, [listObj]);
   const deleteThisList = () => {
     if (window.confirm(`Delete ${listObj.title}?`)) {
       deleteListCards(listObj.firebaseKey).then(() => onUpdate());
@@ -16,6 +22,13 @@ function ListCard({ listObj, onUpdate }) {
     <>
       <Card style={{ width: '18rem', margin: '10px' }}>
         <h3>LIST: {listObj.title}</h3>
+        <ul>
+          {card.map((cards) => (
+            <li key={card.firebaseKey}>
+              {cards.title}
+            </li>
+          ))}
+        </ul>
         <Link href={`/list/${listObj.firebaseKey}`} passHref>
           <Button className="btn btn-danger btn-lg copy-btn" type="button" onClick="">VIEW</Button>
         </Link>
